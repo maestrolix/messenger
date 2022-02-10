b0a4f334e3423f3cea6e1f12ae024d08b1f1ccf0
maestrolix <stepanmurathanov@gmail.com>
2022-02-10 20:07:41 +0300

    Рефакторинг для вебсокетов

--- messenger/config/asgi.py
+++ messenger/config/asgi.py
@@ -1,12 +1,17 @@
 import os
 
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from config.routing import websockets
+from thread.threa_web_socket.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "messenger.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": websockets,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
 })
--- messenger/thread/thread_web_socket/routing.py
+++ messenger/thread/thread_web_socket/routing.py
@@ -4,6 +4,7 @@ from thread.thread_web_socket import consumers
 
 
 websocket_urlpatterns = [
+    path('ws/thread/<int:thread_id>/', consumers.MessageConsumer.as_asgi())
 ]
 
 
