7a4a8c6b63beda78b03e7bc1f033d0c618c36bea
maestrolix <stepanmurathanov@gmail.com>
2022-02-10 19:56:11 +0300

    First commit

--- messenger/account/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from account.models import AdvUser, AvatarImageProfile
-
-admin.site.register(AdvUser)
-admin.site.register(AvatarImageProfile)
-
-
--- messenger/thread/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from thread.models import Thread, Message, MessagePhoto
-
-admin.site.register(MessagePhoto)
-admin.site.register(Thread)
-admin.site.register(Message)
